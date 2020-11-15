158db5c098af0fbbe16e1adc5ac840d8faf140ed
Alex Antoine <antoinealex1234@gmail.com>
2020-11-14 20:20:33 -0500

    javascript complete

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+const _days = document.getElementById('days');
+const _hours = document.getElementById('hours');
+const _minutes = document.getElementById('minutes');
+const _seconds = document.getElementById('seconds');
+
 const newYears = '1 Jan 2021';
 
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 100;
-   
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
     const days = Math.floor(totalSeconds / 3600 / 24);
+ 
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    console.log(days, hours, minutes, seconds);
+    _days.innerHTML = days;
+    _hours.innerHTML = hours;
+    _minutes.innerHTML = minutes;
+    _seconds.innerHTML = seconds;
 }
 
 countdown();
